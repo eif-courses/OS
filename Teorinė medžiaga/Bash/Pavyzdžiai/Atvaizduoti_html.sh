6d6f61c6e4b2186dd17d75e941e61190308dd459
Marius <m.gzegozevskis@gmail.com>
2016-12-01 10:48:17 +0200

    Generuoti HTML tekstą

--- Teorinė medžiaga/Bash/Pavyzdžiai/Atvaizduoti_html.sh
+++ Teorinė medžiaga/Bash/Pavyzdžiai/Atvaizduoti_html.sh
@@ -1,9 +1,59 @@
 #!/bin/bash
 
-read -p "Sukurti pagrindini website puslapi" pav
+read -p "Iveskite puslapio pavadinima:" pav
 
-KOMANDA=`touch $pav`
+pavadinimas=`touch $pav`
 
-echo $KOMANDA
+echo $pavadinimas
 
-echo '<h1>HELLO WORLD</h1>' > $pav
+echo ' ' > $pav
+
+
+echo '<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Paprastas HTML GENERAVIMO PAVYZDYS</h2>
+                                                                                
+  <div class="table-responsive">          
+  <table class="table">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Age</th>
+        <th>City</th>
+        <th>Country</th>
+      </tr>
+    </thead>
+    <tbody>
+      ' >> $pav
+	 
+
+for i in {1..10}; do	 
+	  
+	echo '<tr>' >> $pav
+        echo "<td>$i</td>" >> $pav
+        echo '<td>Anna</td>
+        <td>Pitt</td>
+        <td>35</td>
+        <td>New York</td>
+        <td>USA</td>
+      </tr>' >> $pav
+	  
+    done
+	
+echo '</tbody>
+  </table>
+  </div>
+</div>
+</body>
+</html>' >> $pav
